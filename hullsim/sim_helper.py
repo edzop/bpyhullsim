fd549120ea10a47d7e8799ca8929d0698c11d8d4
ek <ek@toaster>
2021-05-13 14:37:40 +0800

    big rewrite

--- hullsim/sim_helper.py
+++ hullsim/sim_helper.py
@@ -26,6 +26,7 @@ def frame_change_handler(scene):
 
 		if measure_helper.CG_object_name in bpy.data.objects:
 			CG_object=bpy.data.objects[measure_helper.CG_object_name]
+			x_rot=degrees(CG_object.rotation_euler[0])
 			if "displacement_data" in CG_object:
 				displacement_data=CG_object["displacement_data"]
 
@@ -39,7 +40,7 @@ def frame_change_handler(scene):
 							if len(displacement_data)>1:
 								weight=displacement_data[len(displacement_data)-1]
 
-		bouyancy_text_object.data.body="Displacement: %0.02fkg"%(weight)
+		bouyancy_text_object.data.body="Displacement: %0.02fkg\nRoll: %d degrees"%(weight,x_rot)
 	
 def register_text_update_callback():
 	if frame_change_handler not in bpy.app.handlers.frame_change_post:
@@ -62,11 +63,6 @@ class SimSession:
 	force_roll_max=None
 
 	csv_output_filename=None
-
-	# Arm solve threshold for finish solving (pitch and roll)
-	arm_solve_threshold=0.005 # Solve rotation within 5mm arm
-	weight_solve_threshold=5 # solve bouyancy displacement within 5kg
-
 
 	displacement_diff=0
 	cg_empty=None
@@ -91,36 +87,7 @@ class SimSession:
 		self.simulate_roll=simulate_roll
 		self.force_roll_max=force_roll_max
 
-
-
 		self.csv_output_filename=csv_output_filename
-
-
-
-
-	# calculates amount to rotate (around X or Y axis) to solve simulation
-	# larger arm = more movement (faster rotation)
-	# arm = weight X moment
-	def calculate_rotate_step(self,rotate_arm):
-
-		rotate_step=0.05
-
-		abs_rotate_arm=abs(rotate_arm)
-
-		if abs_rotate_arm<0.1:
-			rotate_step=0.01
-		elif abs_rotate_arm<0.5:
-			rotate_step=0.05
-		elif abs_rotate_arm<1:
-			rotate_step=0.1
-		else:
-			rotate_step=0.25
-
-		if rotate_arm<0:
-			rotate_step=-abs(rotate_step)
-
-		return rotate_step
-
 
 	water_volume=None
 	water_displaced_volume=None
@@ -221,6 +188,7 @@ class SimSession:
 			csv_row = []
 
 			csv_row.append("step")
+			csv_row.append("frame")
 
 			csv_row.append("displacement_diff")
 			csv_row.append("self.displaced_weight")
@@ -246,45 +214,54 @@ class SimSession:
 			csv_row = []
 
 			csv_row.append(self.simulation_step) #1
+			csv_row.append(bpy.context.scene.frame_current) #2
 
-			csv_row.append(self.displacement_diff) #2
-			csv_row.append(self.displaced_weight) #3
-			csv_row.append(z_step) #4
+			csv_row.append("%0.01f"%self.displacement_diff) #3
+			csv_row.append("%0.01f"%self.displaced_weight) #4
+			csv_row.append("%0.04f"%z_step) #5
 
-			csv_row.append(self.cg_empty.location.z) #5
+			csv_row.append("%0.04f"%self.cg_empty.location.z) #6
 
-			csv_row.append(degrees(self.cg_empty.rotation_euler.y))  #6 pitch
-			csv_row.append(self.y_arm) #7
-			csv_row.append(y_step) #8
+			csv_row.append("%0.02f"%degrees(self.cg_empty.rotation_euler.y))  #7 pitch
+			csv_row.append("%0.04f"%self.y_arm) #8
+			csv_row.append("%0.04f"%y_step) #9
 
-			csv_row.append(degrees(self.cg_empty.rotation_euler.x)) #9 roll
-			csv_row.append(self.x_arm) #10
-			csv_row.append(x_step) #11
+			csv_row.append("%0.02f"%degrees(self.cg_empty.rotation_euler.x)) #10 roll
+			csv_row.append("%0.04f"%self.x_arm) #11
+			csv_row.append("%0.04f"%x_step) #12
 
 			self.csvWriter.writerow(csv_row)
 
 
-	def report_status(self,z_step):
+	def report_status(self,x_step,y_step,z_step):
 
-		statusText=("s:%d f: %d z: %0.3f dd:%0.3f zs:%0.4f rY:%0.3f Ya:%f rX:%0.3f Xa:%0.4f"%(
+		statusText=("s:%004d f: %004d | Xr:%0.03f Xa:%0.04f Xs:%0.04f | Yr:%0.03f Ya:%0.04f  Ys: %0.04f | Zp: %0.04f Zd:%0.3f Zs:%0.04f"%(
+
 						self.simulation_step,
 						bpy.context.scene.frame_current,
+
+						self.rotationX,
+						self.x_arm,
+						x_step,
+
+						self.rotationY,
+						self.y_arm,
+						y_step,
+
+
 						self.cg_empty.location.z,
 						self.displacement_diff,
 						z_step,
-						self.rotationY,
-						self.y_arm,
-						self.rotationX,
-						self.x_arm
+						
 						))
 
 		print(statusText)
 		
-		bpy.context.workspace.status_text_set(statusText)	
-
 
 	simulation_step=0
 	displacement_data=None
+
+	status_message="-"
 
 
 	def create_bouyancy_text(self):
@@ -310,7 +287,8 @@ class SimSession:
 	def run_simulation(self,limitsteps):
 
 		if self.hull_object.type!="MESH":
-			print("Must select mesh object")
+			self.status_message="Must select mesh object"
+			print(self.status_message)
 			return
 
 		simulation_timer=bpy_helper.ElapsedTimer()
@@ -337,7 +315,12 @@ class SimSession:
 		else:
 			# delete all keyframes so it doesn't interfere with our simulation
 			self.cg_empty.animation_data_clear()
-		
+
+		if self.cg_empty==None:
+			self.status_message="No object selected"
+			print(self.status_message)
+			return
+	
 		# parent it in case it was assigned to different object
 		bpy_helper.parent_objects_keep_transform(self.cg_empty,self.hull_object)
 
@@ -348,6 +331,10 @@ class SimSession:
 		#force_roll_current=0
 
 		#water_volume_phantom=None
+
+		# Bring to center X any Y location
+		self.cg_empty.location.x=0
+		self.cg_empty.location.y=0
 
 		# start hull off above water - start at the height of the hull
 		self.cg_empty.location.z=self.hull_object.dimensions[2]
@@ -374,24 +361,27 @@ class SimSession:
 			# Log results and reporting part of simulation
 			# =======================================================
 
-			self.report_status(z_step)
+			self.report_status(x_step,y_step,z_step)
 
-			if ( (self.force_roll_max==0 and self.displaced_weight>0) or 
-					self.force_roll_max>0):
+			#if ( (self.force_roll_max==0 and self.displaced_weight>0) or 
+			#		self.force_roll_max>0):
+			#	self.write_csv_status(x_step,y_step,z_step)
+
+			if self.simulation_step==0 and self.force_roll_max==0:
+
+				#if self.force_roll_max==0:
+				self.bake_keyframes()
 				self.write_csv_status(x_step,y_step,z_step)
-
-			if self.simulation_step==0:
-
-				if self.force_roll_max==0:
-					self.bake_keyframes()
 			else:
 
 				# To make the resulting output animation smoother we should skip some keyframes frames that are considered subframes in the simulation.
 				# Subframes are not rendered but used for the calculation...
 				# If a forced roll test - only log steps when roll degrees change
 				# If not force roll test - only log when height changes
-				if (self.force_roll_max==0 and z_step>0) or (self.force_roll_max>0 and x_step>0):
+				if (self.force_roll_max==0 and z_step>0) or \
+					(self.force_roll_max>0 and abs(x_step)>0):
 					self.bake_keyframes()
+					self.write_csv_status(x_step,y_step,z_step)
 
 			if self.check_simulation_finished(limitsteps)==True:
 				continue_simulation=False
@@ -408,8 +398,6 @@ class SimSession:
 				z_step=self.process_submersion()
 				
 				self.simulation_step+=1
-
-
 
 
 		# finalize simulation and cleanup
@@ -449,12 +437,22 @@ class SimSession:
 		#x_arm=water_displaced_volume.location.y-cg_world_location[0]	# X
 		#y_arm=water_displaced_volume.location.x-cg_world_location[1]	# Y
 
+		if self.displaced_weight>0:
+			# this is a bit hard to get your head around - flipped from what you think it should be...
+			# - Pitch rotates around the Y axis and the arm for Pitch is measured on the X axis
+			# - Roll rotates around the X axis and the arm for Pitch is measured on the Y axis
+			self.x_arm=displaced_water_cg.x-cg_world_location[1]	# X
+			self.y_arm=displaced_water_cg.y-cg_world_location[0]	# Y
 
-		# this is a bit hard to get your head around - flipped from what you think it should be...
-		# - Pitch rotates around the Y axis and the arm for Pitch is measured on the X axis
-		# - Roll rotates around the X axis and the arm for Pitch is measured on the Y axis
-		self.x_arm=displaced_water_cg.x-cg_world_location[1]	# X
-		self.y_arm=displaced_water_cg.y-cg_world_location[0]	# Y
+			print("cg: (%0.4f %0.4f) displaced(%0.4f,%0.4f)"%(
+				cg_world_location[0],
+				cg_world_location[1],
+				displaced_water_cg.x,
+				displaced_water_cg.y,))
+		else:
+			# if we haven't displaced any water - assume everything is balanced
+			self.x_arm=0
+			self.y_arm=0
 		
 	
 		if self.weightQueue.full():
@@ -465,6 +463,11 @@ class SimSession:
 		# Cache last roll angle to detect roll movement
 		#last_roll_y=degrees(self.hull_object.rotation_euler.y)
 
+	def update_status_message(self,message):
+		self.status_message=message
+		print(self.status_message)
+		bpy.context.workspace.status_text_set(text=message)
+
 
 	def check_simulation_finished(self,limitsteps):
 
@@ -474,12 +477,12 @@ class SimSession:
 
 		# Abort if hull deeper than water volume... something went wrong
 		if self.cg_empty.location.z>self.water_volume.dimensions.z:
-			print("Aborting... Hull Z: %f > water height: %f"%(self.cg_empty.location.z,self.water_volume.dimensions.z))
+			self.update_status_message("Aborting -too deep Hull Z: %f > water height: %f"%(self.cg_empty.location.z,self.water_volume.dimensions.z))
 			return True
 
 		# Abort if runaway...
 		if self.simulation_step>limitsteps:
-			print("Aborting - suspected runaway simulation")
+			self.update_status_message("Aborting - suspected runaway simulation (%d steps)"%limitsteps)
 			return True
 
 		# =======================================================
@@ -491,18 +494,56 @@ class SimSession:
 
 			if ( (self.simulate_pitch and abs(self.x_arm)<self.arm_solve_threshold) or self.simulate_pitch==False) and \
 				( (self.simulate_roll and abs(self.y_arm)<self.arm_solve_threshold) or self.simulate_roll==False) and \
-				( (self.simulate_depth and self.displacement_diff<self.weight_solve_threshold) or self.simulate_depth==False):
-				print("Finished - all parameters stabilized")
+				( (self.simulate_depth and self.displacement_diff<self.weight_solve_threshold) or self.simulate_depth==False):				
+				self.update_status_message("Finished - all parameters stabilized")
 				return True
 		else:
 			# we are doing a forced rollover - 
 			# Abort forced rollover test when we reach the max rollover angle
-			if self.rotationX>=self.force_roll_max:
-					print("Finished - rolltest reached max roll target")
+			if abs(self.rotationX)>=self.force_roll_max:
+					self.update_status_message("Finished - rolltest reached max roll target")
 					return True
 
 		return False
 
+	# Arm solve threshold for finish solving (pitch and roll)
+	arm_solve_threshold=0.005 # Solve rotation within 5mm arm
+	weight_solve_threshold=5 # solve bouyancy displacement within 5kg
+
+	# calculates amount to rotate (around X or Y axis) to solve simulation
+	# larger arm = more movement (faster rotation)
+	# arm = weight X moment
+	def calculate_rotate_step(self,rotate_arm):
+
+		rotate_step=0
+
+		abs_rotate_arm=abs(rotate_arm)
+
+		if abs_rotate_arm>self.arm_solve_threshold*10:
+			rotate_step=0.1
+		elif abs_rotate_arm>self.arm_solve_threshold*5:
+			rotate_step=0.05
+		elif abs_rotate_arm>self.arm_solve_threshold:
+			rotate_step=0.01
+
+
+		#if abs_rotate_arm<0.1:
+		#	rotate_step=0.01
+		#elif abs_rotate_arm<0.5:
+		#	rotate_step=0.05
+		#elif abs_rotate_arm<1:
+		#	rotate_step=0.1
+		#else:
+		#	rotate_step=0.25
+
+
+
+		if rotate_arm>0:
+			rotate_step=-rotate_step
+
+		print("rotate step: %f"%rotate_step)
+
+		return rotate_step
 
 	def process_pitch(self):
 
@@ -522,13 +563,14 @@ class SimSession:
 
 					y_step=self.calculate_rotate_step(self.x_arm)
 
-					self.rotationY+=y_step
+					if y_step!=0:
 
-					#self.hull_object.rotation_euler.y=radians(self.rotationY)
-					self.cg_empty.rotation_euler.y=radians(self.rotationY)
+						self.rotationY+=y_step
+
+						#self.hull_object.rotation_euler.y=radians(self.rotationY)
+						self.cg_empty.rotation_euler.y=radians(self.rotationY)
 
 		return y_step
-
 
 	def process_roll(self):
 
@@ -542,22 +584,24 @@ class SimSession:
 			if ( (self.simulate_depth and self.displacement_diff<self.weight_solve_threshold) or self.simulate_depth==False):
 				# only roll over if pitch is stable
 				if abs(self.x_arm)<self.arm_solve_threshold:
-					x_step=1
+					x_step=-1
 					
-			elif self.simulate_roll==True:
-				# we are NOT doing a forced rollover test
-				# Only rotate once object hits the water
-				if self.displaced_weight>0:
+		else:
+			# we are NOT doing a forced rollover test
 
-					# only roll if greater than threshold
-					if abs(self.y_arm)>self.arm_solve_threshold:
-						x_step=self.calculate_rotate_step(self.y_arm)
+			# Only rotate once object hits the water
+			if self.displaced_weight>0:
 
-		if x_step>0:
-			self.rotationX+=x_step
-			
-			#self.hull_object.rotation_euler.x=radians(self.rotationX)
-			self.cg_empty.rotation_euler.x=radians(self.rotationX)
+				# only roll if greater than threshold
+				if abs(self.y_arm)>self.arm_solve_threshold:
+					x_step=self.calculate_rotate_step(self.y_arm)
+
+		if x_step!=0:
+			if self.simulate_roll==True:
+				self.rotationX-=x_step
+				
+				#self.hull_object.rotation_euler.x=radians(self.rotationX)
+				self.cg_empty.rotation_euler.x=radians(self.rotationX)
 
 		return x_step
 
@@ -593,8 +637,10 @@ class SimSession:
 		queueAverage=0
 
 		for i in range(0,self.weightQueue.qsize()):
-			#print("weight: %f average: %f"%(weightQueue.queue[i][0],weightQueue.queue[i][1]))
+			#print("weight: %f average: %f"%(self.weightQueue.queue[i],queueAverage))
 			queueSum+=self.weightQueue.queue[i]
+			queueAverage=queueSum/(i+1)
+
 
 		# calculate average displacement for last simulation steps
 		# If current displacement is same or similar to queue Average - our velocity is stable
@@ -603,12 +649,18 @@ class SimSession:
 		if self.weightQueue.qsize()>0:
 			queueAverage=queueSum/self.weightQueue.qsize()
 
+		#print("Average: %f limits(%f %f)"%(queueAverage,queueAverage-average_threshold,queueAverage+average_threshold))
+
 		if (queueAverage+average_threshold)<self.weight:
 			#self.hull_object.location.z-=z_step
 			self.cg_empty.location.z-=z_step
+			#print("lower")
 		elif (queueAverage-average_threshold)>self.weight:
 			#self.hull_object.location.z+=z_step
 			self.cg_empty.location.z+=z_step
+			#print("higher")
+		else:
+			z_step=0
 
 		return z_step
 
